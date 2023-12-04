2fb0bf3cd8e080b7b2e2c1737836a3b1cf3e1fba
Luiz Felipe dos Santos <luizfelipesantos.sc@gmail.com>
2023-12-04 16:47:33 +0100

    Added load_data_fifa23 function

---
+++ back-end/packagename/preprocessor.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def load_data_fifa23():
+    '''
+    Function to return the unique players from Fifa 23
+    The most updated version of each player
+    '''
+
+    data = pd.read_csv('../raw_data/male_players_23.csv')
+
+    data_fifa23 = data[data['fifa_version'] == 23]
+
+    data_fifa23 = data_fifa23.drop_duplicates('long_name')
+
+    return data_fifa23
